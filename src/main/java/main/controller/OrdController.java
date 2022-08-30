b86ec943b2ed32ed35dfcfa6a6d40ee7ddf1f3f1
SabinTarba <92083574+SabinTarba@users.noreply.github.com>
2022-08-30 14:07:58 +0300

    Update OrdController.java

--- src/main/java/main/controller/OrdController.java
+++ src/main/java/main/controller/OrdController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://employee-management-starba.netlify.app/")
 @RestController
 @RequestMapping("api/v1/orders/")
 public class OrdController {
